53572fba95aa05463abf86637f58b1e9b0bef699
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-10-17 20:41:00 -0400

    switch command now gets passed args

--- commands/switch.sh
+++ commands/switch.sh
@@ -22,7 +22,7 @@ case $1 in
 	--usage) echo "$COMMAND_NAME {session name}"; exit 0 ;;
 	--valid) echo "NO"; exit 0 ;;
 	--complete) echo "$COMMAND_NAME"; exit 0 ;;
-	--arg) echo "$COMMAND_NAME"; exit 0 ;;
+	--arg) echo "$COMMAND_NAME <args>"; exit 0 ;;
 	--should-list-sessions) echo 1; exit 0 ;;
 	--extra-alfred-items) exit 0 ;;
 	--session-alt-subtitle)
