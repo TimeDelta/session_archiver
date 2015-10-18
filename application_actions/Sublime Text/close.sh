39566724c1860d6996e341e87daceeef1e86bce4
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-10-18 14:00:47 -0400

    made Sublime Text close action robust

--- application_actions/Sublime Text/close.sh
+++ application_actions/Sublime Text/close.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # close the current Sublime project
-# NOTE: this will only work if you add this keyboard shortcut in the system preferences panel:
-#       cmd+alt+shift+ctrl-w
-osascript -e '
-	tell application "Sublime Text" to activate
-	tell application "System Events" to keystroke "w" using {command down, option down, shift down, control down}
-'
+subl --command close_project
+subl --command close_workspace
