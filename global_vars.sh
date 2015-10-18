b84c328751a7144a7d0846c39749055557b86b5a
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-10-17 21:06:03 -0400

    moved current sessions file to /tmp so that system restart clears them

--- global_vars.sh
+++ global_vars.sh
@@ -6,7 +6,7 @@ export WORKFLOW_ID="com.bryanherman.sessionarchiver"
 export ROOT_DIR="`pwd`"
 export COMMANDS_DIR="$ROOT_DIR/commands"
 export SESSIONS_DIR="$ROOT_DIR/sessions"
-export CURRENT_SESSIONS_FILE="$SESSIONS_DIR/.current_sessions"
+export CURRENT_SESSIONS_FILE="/tmp/$WORKFLOW_ID.current_sessions"
 export APPLICATION_ACTIONS_DIR="$ROOT_DIR/application_actions"
 export UTIL="$ROOT_DIR/util"
 
