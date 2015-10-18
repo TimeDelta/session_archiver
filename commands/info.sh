ba38980a28e988c86fb4887903e536264387a4b7
Bryan Herman <TimeDelta@users.noreply.github.com>
2015-10-18 16:33:15 -0400

    info command now allows application extensions to display information

--- commands/info.sh
+++ commands/info.sh
@@ -28,9 +28,30 @@ case $session in
 	--arg) echo "$COMMAND_NAME <args>"; exit 0 ;;
 	--should-list-sessions) echo 1; exit 0 ;;
 	--extra-alfred-items)
-		active="`get_active_sessions`"
-		print_extra_item --valid NO "Active Sessions" "${active:-There are currently no active sessions.}"
-		print_extra_item --valid NO "Inactive Sessions" "`get_inactive_sessions`"
+		if [[ $# -eq 0 ]]; then
+			active="`get_active_sessions`"
+			print_extra_item --valid NO "Active Sessions" "${active:-There are currently no active sessions.}"
+			print_extra_item --valid NO "Inactive Sessions" "`get_inactive_sessions`"
+		else
+			session="$1"
+			shift
+
+			matching_sessions="`get_all_sessions | grep "^$session$"`"
+			if [[ -n $matching_sessions ]]; then
+				session_apps="`get_session_apps "$session"`"
+				app="$*"
+				if [[ -n `echo "$session_apps" | grep "^$app$"` ]]; then
+					run_app_action_for_session "$app" "$COMMAND_NAME" "$session"
+				else
+					IFS=$'\n'
+					for app in `get_session_apps "$session"`; do
+						description="Show this session's info about the saved state of $app"
+						autocomplete="$COMMAND_NAME $session $app"
+						print_extra_item --valid NO --complete "$autocomplete" "$app" "$description"
+					done
+				fi
+			fi
+		fi
 		exit 0 ;;
 	--session-alt-subtitle)
 		session="$1"
